4ad6ac91203720891d6386992249eff53f4df7b6
cryptoeng_vm <Simo>
2023-05-25 17:37:46 +0000

    feat: Hn, keygen, keyregen

--- src/anon-proxy.c
+++ src/anon-proxy.c
@@ -1,6 +1,153 @@
 #include "anon-proxy.h"
 
-int main()
+void anon_proxy_h1(anon_proxy_params_t params,
+                   uint8_t *input, size_t input_size, mpz_t output)
 {
-    return 0;
+    struct anon_proxy_hash_ctx ctx;
+    anon_proxy_hash_ctx_init(&ctx);
+    anon_proxy_hash_ctx_update(&ctx, input, input_size);
+
+    uint8_t digest[anon_proxy_hash_size];
+    anon_proxy_hash_ctx_digest(&ctx, anon_proxy_hash_size, digest);
+
+    mpz_import(output, anon_proxy_hash_size, 1, 1, 0, 0, digest);
+    mpz_mod(output, output, params->p);
+
+    if (mpz_cmp_ui(output, 0) == 0)
+        mpz_set_ui(output, 1);
+}
+
+void anon_proxy_h2(anon_proxy_params_t params, mpz_t input, uint8_t *output)
+{
+    size_t output_size = mpz_sizeinbase(input, 256);
+    uint8_t input_bytes[output_size];
+
+    mpz_export(input_bytes, NULL, 1, 1, 0, 0, input);
+
+    struct anon_proxy_hash_ctx ctx;
+    anon_proxy_hash_ctx_init(&ctx);
+    anon_proxy_hash_ctx_update(&ctx, input_bytes, output_size);
+    anon_proxy_hash_ctx_digest(&ctx, anon_proxy_hash_size, output);
+}
+
+void anon_proxy_h3(anon_proxy_params_t params, mpz_t input, mpz_t output)
+{
+    mpz_mod(output, input, params->p);
+    if (mpz_cmp_ui(output, 0) == 0)
+        mpz_set_ui(output, 1);
+}
+
+// TODO: Same as anon_proxy_h1, maybe unsecure?
+void anon_proxy_h4(anon_proxy_params_t params,
+                   uint8_t *input, size_t input_size, mpz_t output)
+{
+    struct anon_proxy_hash_ctx ctx;
+    anon_proxy_hash_ctx_init(&ctx);
+    anon_proxy_hash_ctx_update(&ctx, input, input_size);
+
+    uint8_t digest[anon_proxy_hash_size];
+    anon_proxy_hash_ctx_digest(&ctx, anon_proxy_hash_size, digest);
+
+    mpz_import(output, anon_proxy_hash_size, 1, 1, 0, 0, digest);
+    mpz_mod(output, output, params->p);
+
+    if (mpz_cmp_ui(output, 0) == 0)
+        mpz_set_ui(output, 1);
+}
+
+void anon_proxy_init(anon_proxy_params_t params,
+                     gmp_randstate_t prng)
+{
+}
+
+void anon_proxy_keygen(anon_proxy_params_t params, gmp_randstate_t prng, anon_proxy_sk_t sk, anon_proxy_pk_t pk)
+{
+    mpz_randomm(sk->sk, prng, params->q);
+    mpz_mul(pk->pk1, params->g, sk->sk);
+
+    size_t sk_size = mpz_sizeinbase(sk->sk, 256);
+
+    uint8_t sk_0[sk_size + 1];
+    uint8_t sk_1[sk_size + 1];
+    mpz_export(sk_0, NULL, 1, 1, 0, 0, sk->sk);
+    mpz_export(sk_1, NULL, 1, 1, 0, 0, sk->sk);
+    sk_0[sk_size] = 0;
+    sk_1[sk_size] = 1;
+
+    mpz_t tmp0;
+    mpz_t tmp1;
+    mpz_inits(tmp0, tmp1, NULL);
+
+    anon_proxy_h4(params, sk_0, sk_size + 1, tmp0);
+    anon_proxy_h4(params, sk_1, sk_size + 1, tmp1);
+
+    mpz_mul(tmp1, sk->sk, tmp1);
+    mpz_add(tmp0, tmp0, tmp1);
+    mpz_powm(pk->pk2, params->g, tmp0, params->p);
+
+    mpz_clears(tmp0, tmp1, NULL);
+}
+
+void anon_proxy_rekeygen(anon_proxy_params_t params, gmp_randstate_t prng, anon_proxy_sk_t sk, anon_proxy_pk_t pk, anon_proxy_rekey_t rekey)
+{
+    mpz_t a1, a2, b1, b2, h4_0, h4_1, tmp;
+    mpz_inits(a1, a2, b1, b2, tmp, NULL);
+
+    size_t sk_size = mpz_sizeinbase(sk->sk, 256);
+    uint8_t sk_0[sk_size + 1];
+    uint8_t sk_1[sk_size + 1];
+    mpz_export(sk_0, NULL, 1, 1, 0, 0, sk->sk);
+    mpz_export(sk_1, NULL, 1, 1, 0, 0, sk->sk);
+    sk_0[sk_size] = 0;
+    sk_1[sk_size] = 1;
+
+    anon_proxy_h4(params, sk_0, sk_size + 1, h4_0);
+    anon_proxy_h4(params, sk_1, sk_size + 1, h4_1);
+    mpz_mod(h4_0, h4_0, params->q);
+    mpz_mod(h4_1, h4_1, params->q);
+
+    do
+    {
+        mpz_randomm(a1, prng, params->q);
+        mpz_randomm(a2, prng, params->q);
+        mpz_mul(tmp, a1, a2);
+        mpz_mod(tmp, tmp, params->q);
+    } while (mpz_cmp_ui(tmp, h4_0) == 0);
+
+    do
+    {
+        mpz_randomm(b1, prng, params->q);
+        mpz_randomm(b2, prng, params->q);
+        mpz_mul(tmp, b1, b2);
+        mpz_mod(tmp, tmp, params->q);
+    } while (mpz_cmp_ui(tmp, h4_1) == 0);
+
+    mpz_clears(h4_0, h4_1, NULL);
+
+    mpz_t r;
+    mpz_init(r);
+    mpz_randomm(r, prng, params->q);
+    size_t r_size = mpz_sizeinbase(r, 256);
+    size_t a2_size = mpz_sizeinbase(a2, 256);
+    size_t b2_size = mpz_sizeinbase(b2, 256);
+
+    uint8_t r_a2_b2[r_size + a2_size + b2_size];
+    mpz_export(r_a2_b2, NULL, 1, 1, 0, 0, r);
+    mpz_export(r_a2_b2 + r_size, NULL, 1, 1, 0, 0, a2);
+    mpz_export(r_a2_b2 + r_size + a2_size, NULL, 1, 1, 0, 0, b2);
+
+    mpz_t h1_output;
+    mpz_init(h1_output);
+
+    anon_proxy_h1(params, r_a2_b2, r_size + a2_size + b2_size, h1_output);
+
+    mpz_set(rekey->rekey1[0], a1);
+    mpz_set(rekey->rekey1[1], b1);
+
+    mpz_powm(rekey->rekey2_1, params->g, h1_output, params->p);
+    mpz_powm(tmp,pk->pk1,h1_output,params->p);
+
+    uint8_t h2_output[anon_proxy_hash_size];
+    anon_proxy_h2(params,tmp,h2_output);
+    //TODO: finish rekeygen
 }
--- src/anon-proxy.h
+++ src/anon-proxy.h
@@ -2,6 +2,14 @@
 #define ANON_PROXY_H
 #include "elgamal-mod.h"
 
+#define anon_proxy_mr_iterations 12
+
+#define anon_proxy_hash_size 32
+#define anon_proxy_hash_ctx sha3_256_ctx
+#define anon_proxy_hash_ctx_init sha3_256_init
+#define anon_proxy_hash_ctx_update sha3_256_update
+#define anon_proxy_hash_ctx_digest sha3_256_digest
+
 struct anon_proxy_params_struct
 {
 
@@ -13,26 +21,51 @@ struct anon_proxy_params_struct
     mpz_t p;
     mpz_t q;
     mpz_t g;
-
-    mpz_t pk;
-    mpz_t sk;
 };
 
 typedef struct anon_proxy_params_struct *anon_proxy_params_ptr;
 typedef struct anon_proxy_params_struct anon_proxy_params_t[1];
 
+struct anon_proxy_sk_struct
+{
+    mpz_t sk;
+};
+typedef struct anon_proxy_sk_struct *anon_proxy_sk_ptr;
+typedef struct anon_proxy_sk_struct anon_proxy_sk_t[1];
+
+struct anon_proxy_pk_struct
+{
+    mpz_t pk1;
+    mpz_t pk2;
+};
+typedef struct anon_proxy_pk_struct *anon_proxy_pk_ptr;
+typedef struct anon_proxy_pk_struct anon_proxy_pk_t[1];
+
+struct anon_proxy_rekey_struct
+{
+    mpz_t rekey1[2];
+    mpz_t rekey2_1;
+    uint8_t *rekey2_2;
+    size_t rekey2_2_size;
+};
+
+typedef struct anon_proxy_rekey_struct *anon_proxy_rekey_ptr;
+typedef struct anon_proxy_rekey_struct anon_proxy_rekey_t[1];
+
 void anon_proxy_h1(anon_proxy_params_t params,
                    uint8_t *input, size_t input_size, mpz_t output);
-void anon_proxy_h2(mpz_t input, uint8_t *output);
 
-void anon_proxy_h3(mpz_t input, mpz_t output);
+void anon_proxy_h2(anon_proxy_params_t params, mpz_t input, uint8_t *output); // Output_size = l == AES_KEY_SIZE == 32
 
-void anon_proxy_h4(uint8_t *input, size_t input_size, mpz_t output);
+void anon_proxy_h3(anon_proxy_params_t params, mpz_t input, mpz_t output);
 
-void anon_proxy_init(anon_proxy_params_t params, elgamal_mod_lambda lambda,
+void anon_proxy_h4(anon_proxy_params_t params, uint8_t *input, size_t input_size, mpz_t output);
+
+void anon_proxy_init(anon_proxy_params_t params,
                      gmp_randstate_t prng);
 
-void anon_proxy_keygen(anon_proxy_params_t params, gmp_randstate_t prng);
+void anon_proxy_keygen(anon_proxy_params_t params, gmp_randstate_t prng, anon_proxy_sk_t sk, anon_proxy_pk_t pk);
 
-void anon_proxy_rekeygen(anon_proxy_params_t params, gmp_randstate_t prng);
+void anon_proxy_rekeygen(anon_proxy_params_t params, gmp_randstate_t prng, anon_proxy_sk_t sk, anon_proxy_pk_t pk, anon_proxy_rekey_t rekey);
+
 #endif // ANON_PROXY_H
