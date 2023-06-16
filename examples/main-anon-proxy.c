e3cf1f21b6a1afd0c59003f9077a608c8ba70d05
cryptoeng_vm <Simo>
2023-06-16 11:48:43 +0000

    refactor: small refactor on example files

--- examples/main-anon-proxy.c
+++ examples/main-anon-proxy.c
@@ -9,44 +9,71 @@ int main()
     gmp_randstate_t prng;
     gmp_randinit_default(prng);
     gmp_randseed_os_rng(prng, 32);
+
+    printf("\n--------INIT--------\n");
     anon_proxy_init(params, prng, anon_proxy_lambda_80);
 
+    printf("\n--------KEYGEN--------\n");
     anon_proxy_sk_t sk_1;
     anon_proxy_pk_t pk_1;
 
     anon_proxy_keygen(params, prng, sk_1, pk_1);
 
+    printf("\n--------PLAINTEXT--------\n");
     anon_proxy_plaintext_t plaintext;
     anon_proxy_plaintext_init_random(prng, plaintext, 16);
+    anon_proxy_plaintext_print(stdout, plaintext);
 
-    pmesg(msg_normal, "\n--------------------------------------------------");
-    pmesg_hex(msg_normal, "plaintext", plaintext->m_size, plaintext->m);
+    printf("\n--------ENCRYPT--------\n");
     anon_proxy_ciphertext_t ciphertext;
-
     anon_proxy_encrypt(params, prng, pk_1, plaintext, ciphertext);
+    anon_proxy_ciphertext_print(stdout, ciphertext);
 
+    printf("\n--------DECRYPT--------\n");
     anon_proxy_plaintext_t plaintext2;
     anon_proxy_decrypt_original(params, sk_1, ciphertext, plaintext2);
+    anon_proxy_plaintext_print(stdout, plaintext2);
 
     assert(plaintext->m_size == plaintext2->m_size);
     assert(memcmp(plaintext->m, plaintext2->m, plaintext->m_size) == 0);
 
+    printf("\n--------KEYGEN2-------\n");
     anon_proxy_sk_t sk_2;
     anon_proxy_pk_t pk_2;
     anon_proxy_keygen(params, prng, sk_2, pk_2);
 
+    printf("\n--------REKEYGEN-------\n");
     anon_proxy_rekey_t rekey;
     anon_proxy_rekeygen(params, prng, sk_1, pk_2, rekey);
+
+    printf("\n--------REENCRYPT-------\n");
     anon_proxy_reencrypted_ciphertext_t reencrypted_ciphertext;
-
     anon_proxy_reencrypt(params, rekey, ciphertext, reencrypted_ciphertext);
+    anon_proxy_reencrypted_ciphertext_print(stdout, reencrypted_ciphertext);
 
+    printf("\n--------DECRYPT2-------\n");
     anon_proxy_plaintext_t plaintext3;
-
     anon_proxy_decrypt_reencrypted(params, sk_2, reencrypted_ciphertext, plaintext3);
+    anon_proxy_plaintext_print(stdout, plaintext3);
+
     assert(plaintext->m_size == plaintext3->m_size);
     assert(memcmp(plaintext->m, plaintext3->m, plaintext->m_size) == 0);
-    printf("Tutto ok\n");
 
+    printf("\n--------CLEAR-------\n");
+
+    anon_proxy_plaintext_clear(plaintext);
+    anon_proxy_plaintext_clear(plaintext2);
+    anon_proxy_plaintext_clear(plaintext3);
+    anon_proxy_ciphertext_clear(ciphertext);
+    anon_proxy_reencrypted_ciphertext_clear(reencrypted_ciphertext);
+    anon_proxy_rekey_clear(rekey);
+    anon_proxy_sk_clear(sk_1);
+    anon_proxy_sk_clear(sk_2);
+    anon_proxy_pk_clear(pk_1);
+    anon_proxy_pk_clear(pk_2);
+    anon_proxy_params_clear(params);
+    gmp_randclear(prng);
+
+    printf("Tutto ok\n");
     return 0;
 }
--- examples/main-elgamal-mod.c
+++ examples/main-elgamal-mod.c
@@ -18,29 +18,26 @@ int main()
     elgamal_mod_init(params, 80, prng);
 
     elgamal_plaintext_t plaintext;
-    plaintext->m_size = 32;
-    plaintext->m = (uint8_t *)malloc(plaintext->m_size * sizeof(uint8_t));
-    for (size_t i = 0; i < plaintext->m_size; i++)
-        plaintext->m[i] = rand() % 256;
+    elgamal_mod_plaintext_init_random(plaintext, prng, 32);
 
-    pmesg(msg_verbose, "\n--------PLAINTEXT--------\n");
-    pmesg_hex(msg_normal, "plaintext", plaintext->m_size, plaintext->m);
+    printf("\n--------PLAINTEXT--------\n");
+    elgamal_mod_plaintext_print(stdout, plaintext);
 
     elgamal_ciphertext_t ciphertext;
-    pmesg(msg_verbose, "\n--------ENCRYPT--------\n");
+    printf("\n--------ENCRYPT--------\n");
     elgamal_mod_encrypt(params, prng, plaintext, ciphertext);
 
-    pmesg_mpz(msg_normal, "c1", ciphertext->c1);
-    pmesg_hex(msg_normal, "c2", ciphertext->c2_size, ciphertext->c2);
+    elgamal_mod_ciphertext_print(stdout, ciphertext);
 
-    plaintext->m_size = 0;
-    free(plaintext->m);
+    elgamal_mod_plaintext_clear(plaintext);
 
-    pmesg(msg_verbose, "\n--------DECRYPT--------\n");
+    printf("\n--------DECRYPT--------\n");
     elgamal_mod_decrypt(params, ciphertext, plaintext);
 
-    pmesg_hex(msg_normal, "plaintext", plaintext->m_size, plaintext->m);
+    elgamal_mod_plaintext_print(stdout, plaintext);
 
-    ciphertext->c2_size = 0;
-    free(ciphertext->c2);
+    elgamal_mod_ciphertext_clear(ciphertext);
+    elgamal_mod_plaintext_clear(plaintext);
+    elgamal_mod_params_clear(params);
+    gmp_randclear(prng);
 }
